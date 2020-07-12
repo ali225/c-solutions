2ca5a786eaf5ed4795b4dbc86efb48422fe25b54
ali225 <aligamal_25@yahoo.com>
2020-07-12 18:35:30 +0200

    delet of element of array

---
+++ delet_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define SZIE 20
+
+void main(void)
+{
+    int data[SZIE];
+    int i,n,pos;
+
+    printf("please Enter size of array > 20 = ");
+    scanf("%d",&n);
+
+    for (i=0 ; i < n; i++)
+    {
+       printf("Value of [%d]",i);
+       scanf("%d",&data[i]);
+    }
+        
+        printf("please Enter pos ");
+        scanf("%d",&pos);
+
+   /* algorithm of instat of array*/
+
+      for(i = pos ; i < n -1; i++)
+      {
+           data[i] = data[i+1];
+      }
+      n--;
+      for(i=0 ; i < n ; i++)
+      {
+        printf("value[%d] is = : %d \n",i, data[i]);
+      }
+
+}
