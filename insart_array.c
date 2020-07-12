47d3f4ca7c83355bc35e907e008ec8b09c9131c5
ali225 <aligamal_25@yahoo.com>
2020-07-12 18:19:39 +0200

    insart of array

--- insart_array.c
+++ insart_array.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
-#define SIZE 20
+#define SZIE 20
+
 void main(void)
 {
-   int data[SIZE]; 
-   int n,i;
-   int newvalue;
-   int post;
+    int data[SZIE];
+    int i,n,newvalue,pos;
 
-   printf("Enter please size of array < 20: ");
-   scanf("%d",&n);
+    printf("please Enter size of array > 20 = ");
+    scanf("%d",&n);
 
-   for(i=0; i < n; i++)
-   {
-    printf("value[%d]:",i);
-    scanf("%d",&data[i]);
-   } 
+    for (i=0 ; i < n; i++)
+    {
+       printf("Value of [%d]",i);
+       scanf("%d",&data[i]);
+    }
+        printf("please enter new value ");
+        scanf("%d",&newvalue);
+        
+        printf("please Enter pos ");
+        scanf("%d",&pos);
 
-   printf("please new item :");
-   scanf("%d",&newvalue);
+   /* algorithm of instat of array*/
 
-   printf("please post :");
-   scanf("%d",&post);
+      for(i = pos ; i >= n-1; i--)
+      {
+           data[i+1] = data[i];
+      }
+      data[pos] = newvalue;
+      n++;
+      for(i=0 ; i < n ; i++)
+      {
+        printf("value[%d] is = : %d \n",i, data[i]);
+      }
 
-   /* algoritm to insert a new item in array */
-
-   for(i=n ; i >= post; i--)
-   {
-     data[i+1] = data[i];
-   }
-
-   data[post] = newvalue;
-   n++;
-
-   for(i=0 ; i<n; i++)
-   {
-    printf("value[%d]: is = %d\n",i,data[i]);
-  
-   }
-   
 }
