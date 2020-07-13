d6a841d3a2ef50153c43bfe1514b93dc254ca4ef
ali225 <aligamal_25@yahoo.com>
2020-07-13 23:41:58 +0200

    insart of element of array

--- insart_array.c
+++ insart_array.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #define SZIE 20
 
 void main(void) {
@@ -19,7 +19,37 @@ void main(void) {
   scanf("%d", &pos);
 
   /* algorithm of instat of array*/
+/*
+for (i = pos; i >= n - 1; i--) {
+  data[i + 1] = data[i];
+}
+data[pos] = newvalue;
+n++;
+for (i = 0; i < n - 1; i++) {
+  printf("value[%d] is = : %d \n", i, data[i]);
+}
+} */
 
+#include <stdio.h>
+#define SIZE 10
+
+void main(void) {
+  int data[SIZE];
+  int i, pos, n, newvalue;
+
+  printf("please Enter size of array > 20 = ");
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    printf("please enter value [%d] :", i);
+    scanf("%d", &data[i]);
+  }
+  // algorithem  delet of array any postion
+
+  printf("please enter new value ");
+  scanf("%d", &newvalue);
+
+  printf("please enter postion :");
+  scanf("%d", &pos);
   for (i = pos; i >= n - 1; i--) {
     data[i + 1] = data[i];
   }
