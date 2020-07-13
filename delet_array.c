31daa2cfecf28d1b33b0510e5a03306aa3db902e
ali225 <aligamal_25@yahoo.com>
2020-07-13 22:17:27 +0200

    delet of element of array

--- delet_array.c
+++ delet_array.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #define SZIE 20
 
 void main(void) {
@@ -16,13 +16,43 @@ void main(void) {
   printf("please Enter pos ");
   scanf("%d", &pos);
 
-  /* algorithm of instat of array*/
+  /* algorithm delet of array*/
+/*
+ for (i = pos; i < n - 1; i++) {
+   data[i] = data[i + 1];
+ }
+ n--;
+ for (i = 0; i < n; i++) {
+   printf("value[%d] is = : %d \n", i, data[i]);
+ }
+} */
 
-  for (i = pos; i < n - 1; i++) {
-    data[i] = data[i + 1];
-  }
-  n--;
+#include <stdio.h>
+#define SIZE 10
+
+void main(void) {
+  int data[SIZE];
+  int i, pos, n;
+
+  printf("please Enter size of array > 20 = ");
+  scanf("%d", &n);
   for (i = 0; i < n; i++) {
-    printf("value[%d] is = : %d \n", i, data[i]);
+    printf("please enter value [%d] :", i);
+    scanf("%d", &data[i]);
+  }
+  // algorithem  delet of array any postion
+
+  printf("please enter postion :");
+  scanf("%d", &pos);
+  if (pos < n - 1) {
+    for (i = pos; i < n - 1; i++) {
+      data[i] = data[i + 1];
+    }
+    n--;
+  } else {
+    printf("the data of array\n");
+    for (i = 0; i < n; i++) {
+      printf("value of array value[%d]:%d\n", i, data[i]);
+    }
   }
 }
